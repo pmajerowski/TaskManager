9c55f60f42e564dbb2f276638a072cc14ec54bbc
pmajerowski <przemyslaw.majerowski@gmail.com>
2022-11-24 23:56:18 +0100

    Most functions in TaskManager implemented. RemoveTask not implemented

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,4 +1,230 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TaskManager {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Path path = Paths.get("tasklist/tasks.csv");
+    private static final String endOfTaskDescription = "$$$_end_of_task_description_$$$";
+    private static final String endOfDate = "$$$_end_of_date_$$$";
+    private static final String regex = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\" +
+            "/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\" +
+            "/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|" +
+            "(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\" +
+            ".)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+
+
+    public static void main(String[] args) {
+        startTaskManager();
+    }
+
+    public static void startTaskManager() {
+        while (true) {
+            switch (printEntryScreen()) {
+                case "ADD", "add" -> addTask();
+                case "RM", "rm" -> removeTask();
+                case "LS", "ls" -> getTaskList();
+                case "Q", "q" -> {
+                    scanner.close();
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println();
+                    System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT + "\t\t\t\tWRONG INPUT!");
+                }
+            }
+        }
+    }
+
+    public static void addTask() {
+        String[] task = new String[3];
+        String important = "";
+
+        printPageScreen("ADD A TASK");
+        System.out.println();
+        System.out.println(ConsoleColors.RESET);
+        System.out.println(ConsoleColors.WHITE_BRIGHT + "\tTask description");
+        System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+        task[0] = scanner.nextLine() + endOfTaskDescription;
+
+        System.out.println();
+        System.out.println(ConsoleColors.WHITE_BRIGHT + "\tTask deadline date [DD-MM-YYYY]");
+        System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+        String date = scanner.nextLine();
+        while (true) {
+            if (validateDate(date)) {
+                task[1] = date + endOfDate;
+                break;
+            } else {
+                System.out.println();
+                System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT + "\t\t\t\tWRONG INPUT!");
+                System.out.println("Type in a valid date in given format: DD-MM-YYYY");
+                System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+                date = scanner.nextLine();
+            }
+        }
+
+        System.out.println();
+        System.out.println(ConsoleColors.WHITE_BRIGHT + "\tMark as important? [Y/N]");
+        System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+
+        important = scanner.nextLine();
+        String cancel = "";
+        while (true) {
+            switch (important) {
+                case "Y", "y" -> task[2] = "isImportant";
+                case "N", "n" -> task[2] = "isNotImportant";
+                default -> {
+                    System.out.println();
+                    System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT + "\t\t\t\tWRONG INPUT!");
+                    System.out.println("Type Y to mark as important, N not to mark, or C to cancel.");
+                    System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+                    important = scanner.nextLine();
+                    if (important.equals("c") || important.equals("C")) {
+                        cancel = "cancel";
+                        break;
+                    }
+                }
+            }
+            if (cancel.equals("cancel")) {
+                startTaskManager();
+            }
+            if (task[2] == "isImportant" || task[2] == "isNotImportant") {
+                break;
+            }
+        }
+        writeToFile(task);
+//        System.out.println();
+//        System.out.println(ConsoleColors.GREEN_BACKGROUND_BRIGHT);
+        System.out.println(ConsoleColors.BLACK_BOLD + "\t\t\tTASK ADDED SUCCESSFULLY!");
+//        System.out.println();
+    }
+
+    public static void removeTask() {
+
+    }
+
+    private static boolean validateDate(String date) {
+
+        if (date.matches(regex)) return true;
+        return false;
+    }
+
+    public static void getTaskList() {
+        printPageScreen("TASKS");
+        try {
+            ArrayList<String> lines = new ArrayList<>();
+            for (String line : Files.readAllLines(path)) {
+                lines.add(line);
+            }
+            if (lines.isEmpty()) {
+                System.out.println();
+                System.out.println();
+                System.out.println("\tYou have no tasks!");
+            }
+
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i).contains("isImportant")) {
+                    System.out.println();
+                    System.out.println(ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.RED_BRIGHT + "\t"
+                            + lines.get(i).substring(0, lines.get(i).indexOf(endOfTaskDescription)));
+                    System.out.print(ConsoleColors.WHITE_BRIGHT + "\t\tDue to "
+                            + lines.get(i).substring(
+                            lines.get(i).indexOf(endOfTaskDescription) + 2
+                                    + endOfTaskDescription.length(), lines.get(i).indexOf(endOfDate)) + "!");
+                    System.out.println("\n. . . . . . . . . . . . . . . . . . . . . . . . . . .");
+                } else if (lines.get(i).contains("isNotImportant")) {
+                    System.out.println(ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.WHITE_BRIGHT + "\t"
+                            + lines.get(i).substring(0, lines.get(i).indexOf(endOfTaskDescription)));
+                    System.out.print(ConsoleColors.WHITE_BRIGHT + "\t\tDue to "
+                            + lines.get(i).substring(lines.get(i).indexOf(endOfTaskDescription) + 2
+                            + endOfTaskDescription.length(), lines.get(i).indexOf(endOfDate)));
+                    System.out.println("\n. . . . . . . . . . . . . . . . . . . . . . . . . . .");
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println(ConsoleColors.YELLOW_BRIGHT + "\tType Q to go back to menu");
+        System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+        String option = scanner.nextLine();
+        while (true) {
+            if (option.equals("Q") || option.equals("q")) {
+                break;
+            } else {
+                System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT + "\t\t\t\tWRONG INPUT!");
+                System.out.println(ConsoleColors.YELLOW_BRIGHT + "\tType Q to go back to menu");
+                System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+                option = scanner.nextLine();
+            }
+        }
+
+    }
+
+    public static String printEntryScreen() {
+        String option = "";
+        System.out.println(ConsoleColors.YELLOW_BACKGROUND_BRIGHT);
+        System.out.println(ConsoleColors.BLACK_BOLD + "\t\t\t\tTASK MANAGER");
+        System.out.println();
+        System.out.println(ConsoleColors.YELLOW + "=============================================");
+        System.out.print(ConsoleColors.YELLOW_BOLD_BRIGHT + "\tChoose an option:");
+        System.out.print(ConsoleColors.YELLOW_BOLD_BRIGHT + "\t\t\t\tType:");
+        System.out.println();
+
+        System.out.print(ConsoleColors.WHITE_BRIGHT + "\tTask list ");
+        System.out.print(ConsoleColors.BLUE + "\t\t\t\t\t\tLS");
+        System.out.println();
+
+        System.out.print(ConsoleColors.WHITE_BRIGHT + "\tAdd a task ");
+        System.out.print(ConsoleColors.BLUE + "\t\t\t\t\t\tADD");
+        System.out.println();
+
+        System.out.print(ConsoleColors.WHITE_BRIGHT + "\tRemove a task ");
+        System.out.print(ConsoleColors.BLUE + "\t\t\t\t\tRM");
+        System.out.println();
+
+        System.out.print(ConsoleColors.WHITE_BRIGHT + "\tExit Task Manager ");
+        System.out.print(ConsoleColors.BLUE + "\t\t\t\tQ");
+        System.out.println();
+        System.out.println();
+        System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+        option = scanner.nextLine();
+
+        return option;
+    }
+
+    public static void printPageScreen(String page) {
+        System.out.println();
+
+        System.out.print(ConsoleColors.YELLOW_BACKGROUND_BRIGHT);
+        System.out.print(ConsoleColors.BLACK_BOLD + "\t" + page);
+
+    }
+
+    public static void writeToFile(String[] task) {
+        try {
+            if (Files.notExists(path)) {
+                Files.createFile(path);
+            } else {
+                String result = "\n".concat(String.join(", ", task));
+                Files.writeString(path, result, StandardOpenOption.APPEND);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/main/java/pl/coderslab/Test.java
@@ -0,0 +1,25 @@
+package pl.coderslab;
+
+import java.util.Scanner;
+
+public class Test {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = "dfghdfhgerrebtb";
+        String b = "aa" + a;
+
+        System.out.println(b);
+        System.out.println(b.substring(0, b.indexOf(a)));
+
+
+
+    }
+
+    public static void printMenu(Scanner scanner) {
+
+    }
+
+
+}

d5bbf84eea9d4c7a84dfed3cd03d23e5fd8e7963
pmajerowski <przemyslaw.majerowski@gmail.com>
2022-11-25 10:21:22 +0100

    improved functionality, remove method implemented

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,6 +1,6 @@
 package pl.coderslab;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +11,8 @@ import java.util.Scanner;
 public class TaskManager {
     private static final Scanner scanner = new Scanner(System.in);
     private static final Path path = Paths.get("tasklist/tasks.csv");
+    private static final String isImportant = "$$$_is_important_$$$";
+    private static final String isNotImportant = "$$$_is_not_important_$$$";
     private static final String endOfTaskDescription = "$$$_end_of_task_description_$$$";
     private static final String endOfDate = "$$$_end_of_date_$$$";
     private static final String regex = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\" +
@@ -78,8 +80,8 @@ public class TaskManager {
         String cancel = "";
         while (true) {
             switch (important) {
-                case "Y", "y" -> task[2] = "isImportant";
-                case "N", "n" -> task[2] = "isNotImportant";
+                case "Y", "y" -> task[2] = isImportant;
+                case "N", "n" -> task[2] = isNotImportant;
                 default -> {
                     System.out.println();
                     System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT + "\t\t\t\tWRONG INPUT!");
@@ -95,19 +97,12 @@ public class TaskManager {
             if (cancel.equals("cancel")) {
                 startTaskManager();
             }
-            if (task[2] == "isImportant" || task[2] == "isNotImportant") {
+            if (task[2] == isImportant || task[2] == isNotImportant) {
                 break;
             }
         }
         writeToFile(task);
-//        System.out.println();
-//        System.out.println(ConsoleColors.GREEN_BACKGROUND_BRIGHT);
-        System.out.println(ConsoleColors.BLACK_BOLD + "\t\t\tTASK ADDED SUCCESSFULLY!");
-//        System.out.println();
-    }
-
-    public static void removeTask() {
-
+        System.out.println(ConsoleColors.GREEN_BACKGROUND + "\t\t\tTASK ADDED SUCCESSFULLY!");
     }
 
     private static boolean validateDate(String date) {
@@ -116,39 +111,127 @@ public class TaskManager {
         return false;
     }
 
+    public static void removeTask() {
+        getTaskList();
+    }
+
+    public static void stolenRemoveLineFromFile(String lineToRemove) {
+
+        try {
+
+            File inFile = new File(String.valueOf(path));
+
+            if (!inFile.isFile()) {
+                System.out.println("No such file!");
+                return;
+            }
+
+            File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
+
+            BufferedReader br = new BufferedReader(new FileReader(String.valueOf(path)));
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+
+                if (!line.trim().contains(lineToRemove)) {
+
+                    pw.println(line);
+                    pw.flush();
+                }
+            }
+            pw.close();
+            br.close();
+
+            if (!inFile.delete()) {
+                System.out.println("Could not delete file");
+                return;
+            }
+
+            if (!tempFile.renameTo(inFile))
+                System.out.println("Could not rename file");
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void removeLineFromFile(String lineToRemove) {
+        ArrayList<String> lines = new ArrayList<>();
+
+        try {
+            for (String line : Files.readAllLines(path)) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            int lineNumber = Integer.parseInt(lineToRemove);
+            if (lineNumber > lines.size() || lineNumber < 1 ) {
+                System.out.println("Wrong value!");
+                System.out.println("Insert index number of task to remove");
+            } else {
+                lines.remove(lineNumber);
+                try {
+                    for (String line : lines) {
+                        Files.writeString(path, line);
+                    }
+                } catch (IOException ex) {
+                    System.out.println("Nie można zapisać pliku.");
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Insert index number of task to remove");
+        }
+
+
+    }
+
+
     public static void getTaskList() {
         printPageScreen("TASKS");
+        System.out.println("");
+        ArrayList<String> lines = new ArrayList<>();
         try {
-            ArrayList<String> lines = new ArrayList<>();
             for (String line : Files.readAllLines(path)) {
                 lines.add(line);
             }
             if (lines.isEmpty()) {
                 System.out.println();
                 System.out.println();
-                System.out.println("\tYou have no tasks!");
+                System.out.println(ConsoleColors.PURPLE_BOLD + "\t\t   You have no tasks!");
             }
 
             for (int i = 0; i < lines.size(); i++) {
-                if (lines.get(i).contains("isImportant")) {
-                    System.out.println();
-                    System.out.println(ConsoleColors.RESET);
+//                System.out.println(ConsoleColors.RESET);
+
+                if (lines.get(i).contains(isImportant)) {
+                    if (i > 0) {
+                        System.out.println("\n. . . . . . . . . . . . . . . . . . . . . . . . . .");
+                    } else System.out.println();
+                    System.out.print(ConsoleColors.YELLOW_BRIGHT + (i + 1) + ")");
                     System.out.println(ConsoleColors.RED_BRIGHT + "\t"
-                            + lines.get(i).substring(0, lines.get(i).indexOf(endOfTaskDescription)));
-                    System.out.print(ConsoleColors.WHITE_BRIGHT + "\t\tDue to "
+                            + lines.get(i).substring(0, lines.get(i).indexOf(endOfTaskDescription)).toUpperCase());
+                    System.out.print(ConsoleColors.WHITE_BRIGHT + "\t\t\t\t\t\t\t\tDue to "
                             + lines.get(i).substring(
                             lines.get(i).indexOf(endOfTaskDescription) + 2
                                     + endOfTaskDescription.length(), lines.get(i).indexOf(endOfDate)) + "!");
-                    System.out.println("\n. . . . . . . . . . . . . . . . . . . . . . . . . . .");
-                } else if (lines.get(i).contains("isNotImportant")) {
-                    System.out.println(ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.RESET);
+
+                } else if (lines.get(i).contains(isNotImportant)) {
+                    if (i > 0) {
+                        System.out.println("\n. . . . . . . . . . . . . . . . . . . . . . . . . .");
+                    } else System.out.println();
+                    System.out.print(ConsoleColors.YELLOW_BRIGHT + (i + 1) + ")");
                     System.out.println(ConsoleColors.WHITE_BRIGHT + "\t"
                             + lines.get(i).substring(0, lines.get(i).indexOf(endOfTaskDescription)));
-                    System.out.print(ConsoleColors.WHITE_BRIGHT + "\t\tDue to "
+                    System.out.print(ConsoleColors.WHITE_BRIGHT + "\t\t\t\t\t\t\t\tDue to "
                             + lines.get(i).substring(lines.get(i).indexOf(endOfTaskDescription) + 2
                             + endOfTaskDescription.length(), lines.get(i).indexOf(endOfDate)));
-                    System.out.println("\n. . . . . . . . . . . . . . . . . . . . . . . . . . .");
                 }
 
             }
@@ -157,15 +240,30 @@ public class TaskManager {
         }
         System.out.println();
         System.out.println();
-        System.out.println(ConsoleColors.YELLOW_BRIGHT + "\tType Q to go back to menu");
+        System.out.println(ConsoleColors.YELLOW_BRIGHT + "\tType Q to go back to menu, \n\tR to remove a task");
         System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
         String option = scanner.nextLine();
         while (true) {
             if (option.equals("Q") || option.equals("q")) {
                 break;
+            } else if (option.equals("R") || option.equals("r")) {
+                if (lines.isEmpty()) {
+                    System.out.println(ConsoleColors.YELLOW_BRIGHT + "\tNothing to remove! \n\tType Q to go back to menu");
+                    System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+                    option = scanner.nextLine();
+
+                } else {
+                    System.out.println("Which task do You want to remove?");
+                    System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
+                    String lineToRemove = scanner.nextLine();
+                    removeLineFromFile(lineToRemove);
+
+                    System.out.println(ConsoleColors.BLUE_BACKGROUND + "\t\t\tTASK SUCCESFULLY REMOVED");
+                    break;
+                }
             } else {
                 System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT + "\t\t\t\tWRONG INPUT!");
-                System.out.println(ConsoleColors.YELLOW_BRIGHT + "\tType Q to go back to menu");
+                System.out.println(ConsoleColors.YELLOW_BRIGHT + "\tType R to remove a task, \n\tQ to go back to menu");
                 System.out.print(ConsoleColors.YELLOW_BRIGHT + "\tINSERT: ");
                 option = scanner.nextLine();
             }
@@ -175,10 +273,11 @@ public class TaskManager {
 
     public static String printEntryScreen() {
         String option = "";
-        System.out.println(ConsoleColors.YELLOW_BACKGROUND_BRIGHT);
+        System.out.println(ConsoleColors.YELLOW + "==============================================");
+        System.out.print(ConsoleColors.YELLOW_BACKGROUND_BRIGHT);
         System.out.println(ConsoleColors.BLACK_BOLD + "\t\t\t\tTASK MANAGER");
-        System.out.println();
-        System.out.println(ConsoleColors.YELLOW + "=============================================");
+//        System.out.println();
+        System.out.println(ConsoleColors.YELLOW + "==============================================");
         System.out.print(ConsoleColors.YELLOW_BOLD_BRIGHT + "\tChoose an option:");
         System.out.print(ConsoleColors.YELLOW_BOLD_BRIGHT + "\t\t\t\tType:");
         System.out.println();
@@ -209,7 +308,7 @@ public class TaskManager {
         System.out.println();
 
         System.out.print(ConsoleColors.YELLOW_BACKGROUND_BRIGHT);
-        System.out.print(ConsoleColors.BLACK_BOLD + "\t" + page);
+        System.out.print(ConsoleColors.BLACK_BOLD + "\t\t\t\t  " + page);
 
     }
 
@@ -218,7 +317,7 @@ public class TaskManager {
             if (Files.notExists(path)) {
                 Files.createFile(path);
             } else {
-                String result = "\n".concat(String.join(", ", task));
+                String result = (String.join(", ", task)).concat("\n");
                 Files.writeString(path, result, StandardOpenOption.APPEND);
             }
 
--- src/main/java/pl/coderslab/Test.java
+++ src/main/java/pl/coderslab/Test.java
@@ -1,18 +1,28 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
+    private static final Path path = Paths.get("tasklist/tasks.csv");
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String a = "dfghdfhgerrebtb";
-        String b = "aa" + a;
-
-        System.out.println(b);
-        System.out.println(b.substring(0, b.indexOf(a)));
-
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            for (String line : Files.readAllLines(path)) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(lines);
+        System.out.println(lines.size());
+        System.out.println(lines.get(1));
 
 
     }
